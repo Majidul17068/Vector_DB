8a7c83baa7246a6c55fe3af9a779f3221b0f1c69
Majidul Islam <83394128+Majidul17068@users.noreply.github.com>
2025-06-02 09:28:28 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 # config.py
 
-PINECONE_API_KEY = "pcsk_48JoKr_TbWVmBTh4tGd7iTJCgASLJvjovazr4Q2FYJujpKLH2sqgWwPfpxpC58kPdaaVJT"
-PINECONE_ENV = "us-east-1"  # e.g., "gcp-starter"
-PINECONE_INDEX_NAME = "n8npdf"
+PINECONE_API_KEY = ""
+PINECONE_ENV = ""  # e.g., "gcp-starter"
+PINECONE_INDEX_NAME = ""
 
-DEEPSEEK_API_KEY = "sk-ed9023ede59b48f9926c9c354f1b5696"
+DEEPSEEK_API_KEY = ""
 
-PDF_PATH = "documents/my_file.pdf"
+PDF_PATH = "documents/YOUR FILE"
 CHUNK_SIZE = 500
 CHUNK_OVERLAP = 50
